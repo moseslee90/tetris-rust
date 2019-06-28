dd6878ebf1452ec685411bf83e30d55a11b7d693
moses <moseslee90@gmail.com>
2019-06-28 20:34:48 +0800

    fixed boundary checking for no_collision for rotations and added lifetime parameter to current_piece in GameVariables struct to enable passing in &Tetronomino

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,9 @@ const PIECE_J: Tetronomino = Tetronomino {
     ],
 };
 
-struct GameVariables {
+struct GameVariables<'a> {
     rotation_state: usize,
-    current_piece: Tetronomino,
+    current_piece: &'a Tetronomino,
     //position of anchor on board [y,x]
     piece_location: [usize; 2],
 }
@@ -55,7 +55,7 @@ fn main() {
     //declare initial rotation state
     let mut game_variables = GameVariables {
         rotation_state: 0usize,
-        current_piece: PIECE_J,
+        current_piece: &PIECE_J,
         piece_location: [0, 0],
     };
 
@@ -66,15 +66,15 @@ fn main() {
     //debugging to test results
     print_game_board(&game_board);
     print_holding_board(&holding_board);
+    move_right(&mut game_variables, &mut game_board);
+    print_game_board(&game_board);
+    move_right(&mut game_variables, &mut game_board);
+    print_game_board(&game_board);
+    move_right(&mut game_variables, &mut game_board);
+    print_game_board(&game_board);
+    move_right(&mut game_variables, &mut game_board);
+    print_game_board(&game_board);
     rotate_piece(&mut game_variables, &mut game_board);
-    print_game_board(&game_board);
-    move_right(&mut game_variables, &mut game_board);
-    print_game_board(&game_board);
-    move_right(&mut game_variables, &mut game_board);
-    print_game_board(&game_board);
-    move_right(&mut game_variables, &mut game_board);
-    print_game_board(&game_board);
-    move_right(&mut game_variables, &mut game_board);
     print_game_board(&game_board);
 }
 
@@ -137,10 +137,10 @@ fn remove_piece(
 
 fn random_tetronomino(game_variables: &mut GameVariables) {
     let random_number = rand::thread_rng().gen_range(1, 3);
-    let spawned_piece: Tetronomino = match random_number {
-        1 => PIECE_L, //choose L piece
-        2 => PIECE_J, //choose J piece
-        _ => PIECE_L,
+    let spawned_piece: &Tetronomino = match random_number {
+        1 => &PIECE_L, //choose L piece
+        2 => &PIECE_J, //choose J piece
+        _ => &PIECE_L,
     };
     game_variables.current_piece = spawned_piece;
     game_variables.piece_location = [SPAWN_Y, SPAWN_X];
@@ -152,8 +152,7 @@ fn rotate_piece(
 ) {
 
     //get current tetronomino
-    remove_piece(game_variables, game_board);
-    let tetronomino: &Tetronomino = &game_variables.current_piece;
+    let tetronomino: &Tetronomino = game_variables.current_piece;
     //get current location of anchor
     let anchor_position_y_start: isize = game_variables.piece_location[0] as isize;
     let anchor_position_x_start: isize = game_variables.piece_location[1] as isize;
@@ -164,27 +163,36 @@ fn rotate_piece(
     let anchor_next_y: isize = tetronomino.template[rotation_state_start][0][0];
     let anchor_next_x: isize = tetronomino.template[rotation_state_start][0][1];
     //find absolute coordinates of next anchor position after rotation on game_board
-    let anchor_position_y_end: usize = if (anchor_position_y_start + anchor_next_y) > 0 {
-        (anchor_position_y_start + anchor_next_y) as usize
-    } else {
-        SPAWN_Y
-    };
-    let anchor_position_x_end: usize = if (anchor_position_x_start + anchor_next_x) > 0 {
-        (anchor_position_x_start + anchor_next_x) as usize
-    } else {
-        SPAWN_X
-    };
-    //update game variables to current state
-    game_variables.piece_location = [anchor_position_y_end, anchor_position_x_end];
-    game_variables.rotation_state = if (game_variables.rotation_state + 1) > 3 {
+    let anchor_position_y_end: usize = (anchor_position_y_start + anchor_next_y) as usize;
+
+    let anchor_position_x_end: usize = (anchor_position_x_start + anchor_next_x) as usize;
+
+    let proposed_location: [usize; 2] = [anchor_position_y_end, anchor_position_x_end];
+
+    let rotation_state_end: usize = if (rotation_state_start + 1) > 3 {
         0
     } else {
-        game_variables.rotation_state + 1
+        rotation_state_start + 1
     };
 
-    //replace piece with next rotation
-    generate_piece(game_variables, game_board);
+    let proposed_variables = GameVariables {
+        rotation_state: rotation_state_end,
+        current_piece: tetronomino,
+        piece_location: proposed_location,
+    };
 
+    if no_collision(proposed_variables, game_board) {
+        remove_piece(game_variables, game_board);
+        //update game variables to current state
+        game_variables.piece_location = [anchor_position_y_end, anchor_position_x_end];
+        game_variables.rotation_state = if (game_variables.rotation_state + 1) > 3 {
+            0
+        } else {
+            game_variables.rotation_state + 1
+        };
+        //replace piece with next rotation
+        generate_piece(game_variables, game_board);
+    }
 }
 
 fn move_right(
@@ -193,7 +201,12 @@ fn move_right(
 ) {
     let mut proposed_location = game_variables.piece_location;
     proposed_location[1] = proposed_location[1] + 1;
-    if no_collision(game_variables, proposed_location, game_board) {
+    let proposed_variables = GameVariables {
+        rotation_state: game_variables.rotation_state,
+        current_piece: game_variables.current_piece,
+        piece_location: proposed_location,
+    };
+    if no_collision(proposed_variables, game_board) {
         remove_piece(game_variables, game_board);
         game_variables.piece_location[1] = game_variables.piece_location[1] + 1;
         //update game_variables
@@ -202,16 +215,16 @@ fn move_right(
 }
 
 fn no_collision(
-    game_variables: &GameVariables,
-    proposed_location: [usize; 2],
+    game_variables: GameVariables,
     game_board: &GameBoard,
 ) -> bool {
     //check out of bounds
     let current_piece = game_variables.current_piece.template;
+    let location = game_variables.piece_location;
     let rotation_state = game_variables.rotation_state;
 
     //check anchor location
-    if proposed_location[0] >= BOARD_HEIGHT || proposed_location[1] >= BOARD_WIDTH {
+    if location[0] >= BOARD_HEIGHT || location[1] >= BOARD_WIDTH {
         return false;
     }
     //print 3 pixels
@@ -219,8 +232,8 @@ fn no_collision(
     let current_template: [[isize; 2]; 4] = current_piece[rotation_state];
     //pixels located from 1 to 3 of array
     for i in 1..4 {
-        let location_y: isize = proposed_location[0] as isize;
-        let location_x: isize = proposed_location[1] as isize;
+        let location_y: isize = location[0] as isize;
+        let location_x: isize = location[1] as isize;
         let pixel_absolute_pos_y: usize = (current_template[i][0] + location_y) as usize;
         let pixel_absolute_pos_x: usize = (current_template[i][1] + location_x) as usize;
 
