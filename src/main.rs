6fc6fcf92e75a4439979ac6a4422494507096550
moses <moseslee90@gmail.com>
2019-06-29 01:37:46 +0800

    consolidated floor_found generate piece and remove piece into one function change_piece to avoid repeated code

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,15 @@ const HOLDING_SIZE: usize = 4;
 
 const SPAWN_X: usize = 4;
 const SPAWN_Y: usize = 17;
+//constants for move_piece function
 const DOWN: &str = "down";
 const RIGHT: &str = "right";
 const LEFT: &str = "left";
+//constants for change_piece function
+const REMOVE_PIECE: &str = "remove_piece";
+const GENERATE_PIECE: &str = "generate_piece";
+const FLOOR_FOUND: &str = "floor_found";
+
 //game_board
 struct GameBoard {
     game_board: [[u8; BOARD_WIDTH]; BOARD_HEIGHT],
@@ -72,7 +78,7 @@ fn main() {
 
     //generate first piece on board
     spawn_tetronomino(&mut game_variables);
-    generate_piece(&game_variables, &mut game_board);
+    change_piece(GENERATE_PIECE, &game_variables, &mut game_board);
     println!("{}", game_variables.rotation_state);
     //debugging to test results
     print_game_board(&game_board);
@@ -96,43 +102,35 @@ fn setup_board(game_board: &mut GameBoard) {
     }
 }
 
-//function used to generate tetronomino based on game_variables
-//assume collision is already handled
-fn generate_piece(
+fn change_piece(
+    change_type: &str,
     game_variables: &GameVariables,
     game_board: &mut GameBoard,
 ) {
+    let mut anchor_value: u8 = 5;
+    let mut pixel_value: u8 = 5;
+    match change_type {
+        REMOVE_PIECE => {
+            anchor_value = 0;
+            pixel_value = 0;
+        }
+        GENERATE_PIECE => {
+            anchor_value = 4;
+            pixel_value = 1;
+        }
+        FLOOR_FOUND => {
+            anchor_value = 2;
+            pixel_value = 2;
+        }
+        _ => (),
+    };
     let current_piece = game_variables.current_piece.template;
     let location = game_variables.piece_location;
     let rotation_state = game_variables.rotation_state;
 
-    //print anchor
-    game_board.game_board[location[0]][location[1]] = 4;
-    //print 3 pixels
-    //find correct template base on rotation_state
-    let current_template: [[i8; 2]; 4] = current_piece[rotation_state];
-    //pixels located from 1 to 3 of array
-    for i in 1..4 {
-        let location_y: i8 = location[0] as i8;
-        let location_x: i8 = location[1] as i8;
-        let pixel_absolute_pos_y: i8 = current_template[i][0] + location_y;
-        let pixel_absolute_pos_x: i8 = current_template[i][1] + location_x;
-
-        game_board.game_board[pixel_absolute_pos_y as usize][pixel_absolute_pos_x as usize] = 1;
-    }
-}
-
-fn remove_piece(
-    game_variables: &GameVariables,
-    game_board: &mut GameBoard,
-) {
-    let current_piece = game_variables.current_piece.template;
-    let location = game_variables.piece_location;
-    let rotation_state = game_variables.rotation_state;
-
-    //remove anchor
-    game_board.game_board[location[0]][location[1]] = 0;
-    //remove 3 pixels
+    //change anchor to fixed state
+    game_board.game_board[location[0]][location[1]] = anchor_value;
+    //change 3 pixels to fixed
     //find correct template based on rotation state
     let current_template: [[i8; 2]; 4] = current_piece[rotation_state];
     //pixels located from 1 to 3 of array
@@ -142,7 +140,8 @@ fn remove_piece(
         let pixel_absolute_pos_y: i8 = current_template[i][0] + location_y;
         let pixel_absolute_pos_x: i8 = current_template[i][1] + location_x;
 
-        game_board.game_board[pixel_absolute_pos_y as usize][pixel_absolute_pos_x as usize] = 0;
+        game_board.game_board[pixel_absolute_pos_y as usize][pixel_absolute_pos_x as usize] =
+            pixel_value;
     }
 }
 
@@ -193,12 +192,12 @@ fn rotate_piece(
 
     if no_collision(proposed_variables, game_board) {
         //remove current piece in present rotation from game_board
-        remove_piece(game_variables, game_board);
+        change_piece(REMOVE_PIECE, game_variables, game_board);
         //update game variables to current state
         game_variables.piece_location = [anchor_position_y_end, anchor_position_x_end];
         game_variables.rotation_state = rotation_state_end;
         //replace piece with next rotation
-        generate_piece(game_variables, game_board);
+        change_piece(GENERATE_PIECE, game_variables, game_board);
     }
 }
 
@@ -226,10 +225,12 @@ fn move_piece(
         piece_location: proposed_location,
     };
     if no_collision(proposed_variables, game_board) {
-        remove_piece(game_variables, game_board);
-        game_variables.piece_location = proposed_location;
+        //remove piece before moved state
+        change_piece(REMOVE_PIECE, game_variables, game_board);
         //update game_variables
-        generate_piece(game_variables, game_board);
+        game_variables.piece_location = proposed_location;
+        //generate piece in new moved state
+        change_piece(GENERATE_PIECE, game_variables, game_board);
     }
 }
 
